d89cacd3b9730c429356d5952668c8d228c9f52f
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-10-07 11:28:44 -0400

    Adding Employees

--- src/comp/comp152/Main.java
+++ src/comp/comp152/Main.java
@@ -1,14 +1,29 @@
 package comp.comp152;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        WeekDay deliveryDay = WeekDay.Friday;
+
+        var allEmployees = new ArrayList<Employee>();
+
+        Employee newEmployee = new CommissionEmployee();
+        allEmployees.add(newEmployee);
+
+        newEmployee = new SalariedEmployee(55000);
+        allEmployees.add(newEmployee);
+        //add more employees as needed
+
+        for(var emp : allEmployees){
+            System.out.println("Paying a " + emp.getClass().getName() + " $" + emp.calculateBiWeeklyPay());
+        }
+
+    /* WeekDay deliveryDay = WeekDay.Friday;
         var picker = new Random();
         var choice = picker.nextInt(WeekDay.values().length);
         deliveryDay = WeekDay.values()[choice];
-        System.out.println("Your delivery will happen on " + deliveryDay + ".");
+        System.out.println("Your delivery will happen on " + deliveryDay + "."); */
     }
 }
